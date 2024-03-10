0ad627ee86f1917ab5a7e1e76c062808301d5e6a
UrsuTudor <tudor_ursu90@yahoo.com>
2024-03-10 11:02:50 +0200

    Solve exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,24 @@
-const repeatString = function() {
-
+const repeatString = function(string, n) {
+    let strings = ''
+    let i = 0
+    while(i < n) {
+        strings += string
+        i++
+    }
+    while (i > n) {
+        strings += 'ERROR'; 
+        break
+    }
+    
+    
+    return(strings)
+    //need the function to repeat a string for a specified number of times 
+    //need a value to store the string, because I need to add the repeated string to it 
+    //need a loop function that will iterate until I get to n 
 };
+
+repeatString('hey', 10)
+
 
 // Do not edit below this line
 module.exports = repeatString;
