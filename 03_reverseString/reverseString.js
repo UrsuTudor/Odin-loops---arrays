710986e3d63cb7438a154781b53480b729e9a466
UrsuTudor <tudor_ursu90@yahoo.com>
2024-03-12 22:37:18 +0200

    Solve exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringElements = []
+    for (i = 0; i < string.length; i++) {
+       stringElements[i] = string.charAt(i)
+    } 
+    let reversedStringElements = stringElements.reverse()
+    let reversedString = reversedStringElements.join('')
+    
+    return reversedString
 };
 
 // Do not edit below this line
