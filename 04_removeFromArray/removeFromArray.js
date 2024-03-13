1fd20f7639dcbfbacb348cc2c7d1fc7b2eb23968
UrsuTudor <tudor_ursu90@yahoo.com>
2024-03-13 19:21:10 +0200

    Solve exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...elements) {
+    const filteredArray = []
+    array.forEach((item) => {
+        if(!elements.includes(item)){
+            filteredArray.push(item)
+        }
+    })
+    return filteredArray
+}
 
-};
+//to remove an item from an array
+//go through each element of the array and check if it matches any of the parameters in the parameter array
+//if it does not, push it into a new array
 
 // Do not edit below this line
 module.exports = removeFromArray;
