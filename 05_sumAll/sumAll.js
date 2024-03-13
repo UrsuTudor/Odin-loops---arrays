c48ad8bc25511664aab1da54c68178f0f741fac5
UrsuTudor <tudor_ursu90@yahoo.com>
2024-03-13 20:45:36 +0200

    Solve exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(...numbers) {
+    let sum = 0
+    let ascendingNumbers = numbers.sort(function(a, b){return a - b})
+    let i = ascendingNumbers[0]
+    let y = ascendingNumbers[1]
+    if(i < 0) {
+        return 'ERROR'
+    } else if (Number(i) !== i) {
+        return 'ERROR'
+    } else if (Number(y) !== y) {
+        return 'ERROR'
+    }
+    while (i <= ascendingNumbers[1]) {
+        sum += i
+        i++
+    }
+    return sum
 };
+
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
