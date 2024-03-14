6ba67f1378cdee027932fdb8a2b508525cd79687
UrsuTudor <tudor_ursu90@yahoo.com>
2024-03-14 18:52:27 +0200

    Solve exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let tempCelsius = ((fahrenheit - 32) * 5) / 9
+  tempCelsius = parseFloat(tempCelsius.toFixed(1))
+  //celsius = Math.round(celsius * 10) / 10
+  return tempCelsius
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let tempFahrenheit = (celsius * 9) /5 + 32
+  tempFahrenheit = parseFloat (tempFahrenheit.toFixed(1))
+  return tempFahrenheit
 };
 
 // Do not edit below this line
