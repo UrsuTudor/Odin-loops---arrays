4542c77a5a771dd06c23880a52829bb7778e5439
UrsuTudor <tudor_ursu90@yahoo.com>
2024-04-02 18:52:23 +0300

    Solve exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const arryedString = Array.from(string);
+    const lettersInString = arryedString.filter((character) => (character >='a' && character <= 'z') || 
+                                                                (character >='A' && character <= 'Z') || 
+                                                                (character >= 0 || character <= 0));
+    
+    let removedSpaces = lettersInString.filter((character) => character !== ' ')
+    let removedSpacesReversed = removedSpaces.toReversed()
 
+    removedSpaces = removedSpaces.join('').toLowerCase()
+    removedSpacesReversed = removedSpacesReversed.join('').toLowerCase()
+    
+    if (removedSpaces === removedSpacesReversed) {
+        return true
+    } else {return false}
+    
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
