cb6750a4aa1da2a97ebe0f5e83b8ae4077c08d4a
UrsuTudor <tudor_ursu90@yahoo.com>
2024-04-02 17:38:41 +0300

    Solve exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, num) => {return total + num} , 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, num) => {return total * num} , 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let smallerInt = []
+  for (i = 0; i <= a; i++){
+    smallerInt[i] = i;
+  }
+
+  let product = 1;
+  for (i = 1; i < smallerInt.length; i++){
+    product *= smallerInt[i];
+  }
+  return product;
 };
 
 // Do not edit below this line
