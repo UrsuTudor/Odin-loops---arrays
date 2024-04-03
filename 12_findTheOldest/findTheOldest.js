7cfedd98a2ca382dec2510daf2251c7499cf0377
UrsuTudor <tudor_ursu90@yahoo.com>
2024-04-03 20:05:17 +0300

    Solve exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let ascendingByAge = arr.sort((person1, person2) => {
+        if (person1.yearOfDeath == undefined) {person1.yearOfDeath = new Date().getFullYear()};
+        if (person2.yearOfDeath == undefined) {person2.yearOfDeath = new Date().getFullYear()};
+        return  (person1.yearOfDeath - person1.yearOfBirth) - (person2.yearOfDeath - person2.yearOfBirth);
+    })
 
+    let oldestPerson = ascendingByAge.pop();
+    return oldestPerson;
 };
 
 // Do not edit below this line
